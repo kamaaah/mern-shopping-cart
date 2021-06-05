ef6ffc00c349129976ac727bb3d3528db1c6e2b8
kmaadteck <x.argis@gmail.com>
2021-06-05 11:25:05 +0200

    Make product's grid fully responsive

---
+++ frontend/src/pages/Cart/Cart.js
@@ -0,0 +1,11 @@
+import './Cart.css'
+
+const Cart = () => {
+    return (
+        <div className="cartscreen">
+            Cart Screen
+        </div>
+    )
+}
+
+export default Cart
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,20 @@
+import "./Home.css"
+import Products from '../Products/Product'
+const Home = () => {
+    return (
+        <div className="homescreen">
+          <h2 className="homescreen__title">Latest Products</h2>
+          <div className="homescreen__products">
+              <Products />
+              <Products />
+              <Products />
+              <Products />
+              <Products />
+              <Products />
+
+          </div>
+        </div>
+    )
+}
+
+export default Home
