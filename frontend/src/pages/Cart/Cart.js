ea4b1aeb422fd61d0843595d95a71e00009a5adf
kmaadteck <x.argis@gmail.com>
2021-06-06 11:25:03 +0200

    CartItem component DONE

--- frontend/src/pages/Cart/Cart.js
+++ frontend/src/pages/Cart/Cart.js
@@ -1,9 +1,24 @@
 import './Cart.css'
+import CartItem from "../../components/CartIem/CartItem";
+
 
 const Cart = () => {
+
     return (
-        <div className="cartscreen">
-            Cart Screen
+        <div className="cartpage">
+           <div className="cartpage__left">
+               <h2>Shopping Cart</h2>
+               <CartItem />
+           </div>
+           <div className="cartpage__right">
+               <div className="cartpage__info">
+                   <p>Subtotal (0) items</p>
+                   <p>$599.99</p>
+               </div>
+               <div>
+                   <button>Proceed To Checkout</button>
+               </div>
+           </div>
         </div>
     )
 }
